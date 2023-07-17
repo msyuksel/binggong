001117bcd70cc7e110aaf1939191ca01de2c8210
msyuksel <55067213+msyuksel@users.noreply.github.com>
2023-07-17 17:24:29 -0500

    Update AddExerciseScene.js

--- AddExerciseScene.js
+++ AddExerciseScene.js
@@ -88,25 +88,6 @@ const AddExerciseScene = () => {
     );
   };
 
-  // A function to filter the exercises by the search input and the selected tab
-  const filterExercises = () => {
-    // Filter the exercises by the search input
-    let filteredExercises = exercises.filter((exercise) =>
-      exercise.name.toLowerCase().includes(search.toLowerCase())
-    );
-
-    // Filter the exercises by the selected tab
-    if (tab === "All") {
-      return filteredExercises;
-    } else if (tab === "Previous Exercises") {
-      // TODO: Implement logic to get previous exercises from local storage or database
-      return [];
-    } else if (tab === "Custom Exercises") {
-      // TODO: Implement logic to get custom exercises from local storage or database
-      return [];
-    }
-  };
-
   // A function to render the list of exercises based on the filter function
   const renderList = () => {
     // Render a different header based on the selected tab
@@ -123,13 +104,39 @@ const AddExerciseScene = () => {
       );
     }
 
+    // A function to filter the exercises by the search input and the selected tab
+    const filterExercises = () => {
+      // Filter the exercises by the search input
+      let filteredExercises = exercises.filter((exercise) =>
+        exercise.name.toLowerCase().includes(search.toLowerCase())
+      );
+
+      // Filter the exercises by the selected tab
+      if (tab === "All") {
+        return filteredExercises;
+      } else if (tab === "Previous Exercises") {
+        // TODO: Implement logic to get previous exercises from local storage or database
+        return [];
+      } else if (tab === "Custom Exercises") {
+        // TODO: Implement logic to get custom exercises from local storage or database
+        return [];
+      }
+    };
+
     // Render the flat list of exercises with the header and the item component
     return (
       <FlatList
-        data={filterExercises()}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => <ExerciseItem item={item} />}
-        // Here you are rendering the listHeader variable directly, without wrapping it in a Text component
+        data={filterExercises}
+        renderItem={({ item }) => (
+          console.log(item); // Print the item object for debugging
+          <ExerciseItem
+            name={item?.name} // Use the optional chaining operator to access the name property safely
+            force={item?.force} // Use the optional chaining operator to access the force property safely
+            primaryMuscle={[item?.primaryMuscles]} // Use the optional chaining operator to access the primaryMuscles property safely
+            secondaryMuscle={[item?.secondaryMuscle]} // Use the optional chaining operator to access the secondaryMuscle property safely
+          />
+        )}
+        keyExtractor={(item) => item.name}
         ListHeaderComponent={listHeader}
       />
     );
